bb8869440ea65816b1eb26fd8f81588eec662404
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2020-04-28 16:25:56 +0900

    update to ubuntu20.04

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
-for tag in dorowu-bionic #18.04
+for tag in 20.04 latest #18.04
 do
     docker build -t rnakato/ubuntu:$tag .
-#    docker push rnakato/ubuntu:$tag
+    docker push rnakato/ubuntu:$tag
+done
+
+for tag in #dorowu-bionic
+do
+    docker build -f Dockerfile.dorowu -t rnakato/ubuntu:$tag .
+    docker push rnakato/ubuntu:$tag
 done
