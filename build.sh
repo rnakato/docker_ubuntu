7fcb236e3f23d683433179dd9bcf7b3cdb775249
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2020-06-10 20:25:43 +0900

    add GPU

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
+for tag in latest pytorch-1.5-cuda10.1-cudnn7-devel #latest 10.1-cudnn7-devel-ubuntu18.04
+do
+    docker build -f Dockerfile.GPU -t rnakato/ubuntu_gpu:$tag .
+    docker push rnakato/ubuntu_gpu:$tag
+done
+exit
+
 for tag in 20.04 latest #18.04
 do
     docker build -t rnakato/ubuntu:$tag .
