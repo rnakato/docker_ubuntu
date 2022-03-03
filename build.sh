00883705910d27bb0a7319877d509722bf3a841e
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2022-03-03 14:08:26 +0900

    change base image from pytorch to nvidia-cuda

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-for tag in latest pytorch-1.10.0-cuda11.3-cudnn8-devel #pytorch-1.5-cuda10.1-cudnn7-devel
+for tag in nvidia-11.5.1-cudnn8-devel-ubuntu20.04 latest #pytorch-1.10.0-cuda11.3-cudnn8-devel #pytorch-1.5-cuda10.1-cudnn7-devel
 do
     docker build -f Dockerfile.GPU -t rnakato/ubuntu_gpu:$tag .
     docker push rnakato/ubuntu_gpu:$tag
