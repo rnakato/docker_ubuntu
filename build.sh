05a496bcd2e91f902e7d76e0ad80585560921a4f
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2022-08-17 21:08:27 +0900

    update

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
-for tag in latest 11.3.1-cudnn8-runtime-ubuntu20.04 #11.4.2-cudnn8-runtime-ubuntu20.04 #nvidia-11.5.1-cudnn8-devel-ubuntu20.04
+for tag in latest 2022.08 #11.4.2-cudnn8-runtime-ubuntu20.04 # 11.3.1-cudnn8-runtime-ubuntu20.04 #nvidia-11.5.1-cudnn8-devel-ubuntu20.04
 do
     docker build -f Dockerfile.GPU -t rnakato/ubuntu_gpu:$tag .
     docker push rnakato/ubuntu_gpu:$tag
 done
 
-for tag in 20.04 latest #18.04
+for tag in latest 2022.08 #20.04 #18.04
 do
     docker build -t rnakato/ubuntu:$tag .
     docker push rnakato/ubuntu:$tag
