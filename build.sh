b973b78f1d636d73218841c4e9354ed87de7d88c
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2026-03-13 22:39:07 +0900

    Update: 2026.03

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
-for version in 22.04 #20.04
+for version in #22.04 #20.04
 do
     for tag in 2026.03 latest
     do
 #        docker buildx build -f Dockerfile.$version -t rnakato/ubuntu_$version:test --target normal .
 #	exit
         docker build -f Dockerfile.$version -t rnakato/ubuntu_$version:$tag --target normal .
+
         docker push     rnakato/ubuntu_$version:$tag
         docker build -f Dockerfile.$version -t rnakato/ubuntu_gpu_$version:$tag --target gpu . #--no-cache
         docker push     rnakato/ubuntu_gpu_$version:$tag
@@ -12,7 +13,7 @@ do
 done
 
 
-exit
+#exit
 #docker build -f Dockerfile.20.04 -t rnakato/ubuntu_gpu_20.04:cuda11.0.3-cudnn8 --target gpu_juicer .
 #docker push rnakato/ubuntu_gpu_20.04:cuda11.0.3-cudnn8
 
